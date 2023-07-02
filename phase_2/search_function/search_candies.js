011405c1b7639ccfd8a6a2404fa087efc89bc2e6
Anthony O <hackerman.ant@gmail.com>
2023-07-02 17:41:02 +0100

    Search function completed

---
+++ phase_2/search_function/search_candies.js
@@ -0,0 +1,36 @@
+const candies = [
+  { name: 'Aero', price: 1.99 },
+  { name: 'Skittles', price: 2.99 },
+  { name: 'Mars', price: 1.49 },
+  { name: 'Maltesers', price: 3.49 },
+  { name: 'Skittles', price: 1.49 },
+  { name: 'Starburst', price: 5.99 },
+  { name: 'Ricola', price: 1.99 },
+  { name: 'Polkagris', price: 5.99 },
+  { name: 'Pastila', price: 4.99 },
+  { name: 'Mentos', price: 8.99 },
+  { name: 'Raffaello', price: 7.99 },
+  { name: 'Gummi bears', price: 10.99 },
+  { name: 'Fraise Tagada', price: 5.99 }
+];
+
+const searchCandies = (String, Number) => {
+  
+  // let splitString = String.split('');
+  // splitString[0].toUpperCase();
+  // console.log(splitString);
+  // let uppercasedName = splitString.join('');
+  // console.log(uppercasedName);
+
+  uppercasedName = 
+    String.charAt(0).toUpperCase()
+    + String.slice(1)
+
+  let result = 
+  candies.filter(candy => candy.name.startsWith(uppercasedName) && Number >= candy.price);
+
+  let candyList = result.map(candy => candy.name);
+  return candyList;
+};
+
+module.exports = searchCandies;
