959e603455919e731c55c74224919783f1ee2321
Anthony O <hackerman.ant@gmail.com>
2023-08-23 13:26:36 +0100

    candy and shopping basket class complete

---
+++ phase_2/shopping_basket/shopping_basket.js
@@ -0,0 +1,21 @@
+class ShoppingBasket {
+
+  constructor() {
+    this.basket = [];
+  }
+
+  addItem(candy) { 
+    this.basket.push(candy);
+  }
+
+  getTotalPrice() {
+    let totalPrice = 0;
+    this.basket.forEach((candy) => {
+      totalPrice += candy.getPrice();
+    });
+
+    return totalPrice;
+  }
+}
+
+module.exports = ShoppingBasket;
