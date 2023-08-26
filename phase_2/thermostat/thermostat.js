677a2daa88ccdf0e4e25ce06737b79e2746e0f48
Anthony O <hackerman.ant@gmail.com>
2023-08-26 23:29:53 +0100

    up method partially complete

--- phase_2/thermostat/thermostat.js
+++ phase_2/thermostat/thermostat.js
@@ -1,11 +1,19 @@
 class Thermostat {
 
   constructor() {
-    this.temperature = 20
+    this.temperature = 20;
   }
 
   getTemperature() {
-    return this.temperature
+    return this.temperature;
+  }
+
+  up(degreesUp) {
+    if(degreesUp === undefined) {
+      this.temperature += 1;
+    } else {
+      this.temperature += degreesUp;
+    }
   }
 }
 
