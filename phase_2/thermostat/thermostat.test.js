87a4e9489fbc461b7ea91a763e650d36e3337315
Anthony O <hackerman.ant@gmail.com>
2023-08-26 23:28:53 +0100

    tests for up method

--- phase_2/thermostat/thermostat.test.js
+++ phase_2/thermostat/thermostat.test.js
@@ -5,4 +5,17 @@ describe('Thermostat', () =>{
     const thermostat = new Thermostat();
     expect(thermostat.getTemperature()).toEqual(20)
   });
+
+  it('Increases the temperature by 1 degree celcius', () => {
+    const thermostat = new Thermostat();
+    thermostat.up();
+    expect(thermostat.getTemperature()).toEqual(21)
+  });
+
+  it('increases the temperature by 2 degrees celcius', () => {
+    const thermostat = new Thermostat();
+    thermostat.up(2);
+    expect(thermostat.getTemperature()).toEqual(22)
+  });
 });
+
