9e41f0ea843027a29f7c52b1a91966c2b7543126
Anthony O <hackerman.ant@gmail.com>
2023-08-27 01:04:45 +0100

    down method partially completed

--- phase_2/thermostat/thermostat.js
+++ phase_2/thermostat/thermostat.js
@@ -15,6 +15,19 @@ class Thermostat {
       this.temperature += degreesUp;
     }
   }
+
+  down(degreesDown) {
+    if(this.temperature - degreesDown <= 10) {
+      let message = 'Temperature cannot be set below 10 degrees celcius'
+      return message
+    } else {
+      if(degreesDown === undefined) {
+        return this.temperature -= 1;
+      } else {
+        return this.temperature -= degreesDown;
+      }
+    }
+  }
 }
 
 module.exports = Thermostat;
