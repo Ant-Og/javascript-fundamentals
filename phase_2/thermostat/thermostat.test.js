30a8ccfbf925f69896ac1d8d10dff9d34771d23c
Anthony O <hackerman.ant@gmail.com>
2023-08-27 01:03:55 +0100

    tests for down method

--- phase_2/thermostat/thermostat.test.js
+++ phase_2/thermostat/thermostat.test.js
@@ -17,5 +17,23 @@ describe('Thermostat', () =>{
     thermostat.up(2);
     expect(thermostat.getTemperature()).toEqual(22)
   });
+
+  it('decreases the temperature by 1 degree celcius', () => {
+    const thermostat = new Thermostat();
+    thermostat.down();
+    expect(thermostat.getTemperature()).toEqual(19)
+  });
+
+  it('decreases the temperature by 2 degrees celcius', () => {
+    const thermostat = new Thermostat();
+    thermostat.down(2);
+    expect(thermostat.getTemperature()).toEqual(18)
+  });
+
+  it('cannot decrease the temperature below 10 degrees celcius', () => {
+    const thermostat = new Thermostat();
+    expect(thermostat.down(11)).toEqual('Temperature cannot be set below 10 degrees celcius')
+    expect(thermostat.getTemperature()).toEqual(20)
+  });
 });
 
