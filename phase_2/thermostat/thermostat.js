aa354ccdf94a47948be3d71e580ffeeb9fa36f20
Anthony O <hackerman.ant@gmail.com>
2023-08-29 16:04:41 +0100

    PSM, up and down methods completed

--- phase_2/thermostat/thermostat.js
+++ phase_2/thermostat/thermostat.js
@@ -1,7 +1,12 @@
+const { on } = require("events");
+
 class Thermostat {
 
   constructor() {
     this.temperature = 20;
+    this.powerSavingMode = true;
+    this.maxTemp = 25;
+    this.minTemp = 10;
   }
 
   getTemperature() {
@@ -9,23 +14,42 @@ class Thermostat {
   }
 
   up(degreesUp) {
-    if(degreesUp === undefined) {
-      this.temperature += 1;
-    } else {
+    if (this.temperature + degreesUp < this.maxTemp) {
       this.temperature += degreesUp;
+      let message = `The temperature has been set to ${this.maxTemp} degrees celcius`
+      return message
+    } else {
+      this.temperature = this.maxTemp //25 degrees
+      let message = `The temperature has been set to ${this.maxTemp} degrees celcius and cannot be set above 25 degrees celcius in PSM, or above 32 degrees celcius when PSM is off`;
+      return message;
     }
   }
 
   down(degreesDown) {
     if(this.temperature - degreesDown <= 10) {
-      let message = 'Temperature cannot be set below 10 degrees celcius'
-      return message
+      this.temperature = this.minTemp
+      console.log(this.temperature)
+      let message = 'The temperature has been set to 10 degrees celcius and cannot be set below';
+      return message;
     } else {
-      if(degreesDown === undefined) {
-        return this.temperature -= 1;
-      } else {
-        return this.temperature -= degreesDown;
-      }
+      this.temperature -= degreesDown;
+      let message = `The temperature has been set to ${this.minTemp} degrees celcius`
+      return message;
+    }
+  }
+
+  setPowerSavingModeOnOff(value) {
+    console.log(this.maxTemp)
+    if(value === 'on') {
+      this.powerSavingMode = true;
+      this.maxTemp = 25;
+      let message = 'PSM is on'
+      return message
+    } else if(value === 'off') {
+      this.powerSavingMode = false;
+      this.maxTemp = 32;
+      let message = 'PSM is off'
+      return message
     }
   }
 }
