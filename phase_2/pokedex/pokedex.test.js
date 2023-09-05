ade7e773198572bb779fc7e255610330427dc210
Anthony O <hackerman.ant@gmail.com>
2023-09-05 03:05:20 +0100

    test for Pokedex class complete

--- phase_2/pokedex/pokedex.test.js
+++ phase_2/pokedex/pokedex.test.js
@@ -1,13 +1,20 @@
-const fetchPokemon = require('./api');
+const Pokedex = require('./pokedex');
 
-describe('fetchPokemon', () => {
-  it('returns 5 attributes for a given pokemon', () => {
-    return fetchPokemon('pikachu').then((data) => {
-      expect(data.name).toEqual('pikachu');
-      expect(data.id).toEqual(25);
-      expect(data.height).toEqual(4);
-      expect(data.weight).toEqual(60);
-      expect(data.type).toEqual('electric');
-    });
+describe('Pokedex', () => {
+  it('returns a list of pokemon that the user has caught in their pokedex', async () => {
+    const pokedex = new Pokedex();
+    await pokedex.catch('pikachu');
+    await pokedex.catch('jigglypuff');
+    await pokedex.catch('charizard');
+    await pokedex.catch('mew');
+    await pokedex.catch('mewtwo');
+    const expectedObject = [
+      { name: 'pikachu', id: 25, height: 4, weight: 60, type: 'electric' },
+      { name: 'jigglypuff', id: 39, height: 5, weight: 55, type: 'normal' },
+      { name: 'charizard', id: 6, height: 17, weight: 905, type: 'fire' },
+      { name: 'mew', id: 151, height: 4, weight: 40, type: 'psychic' },
+      { name: 'mewtwo', id: 150, height: 20, weight: 1220, type: 'psychic' }
+    ];
+    expect(pokedex.all()).toEqual(expectedObject);
   });
 });
