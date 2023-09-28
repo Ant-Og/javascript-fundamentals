5486e5f335196305696bc97da4b7c2fbdc940e78
Anthony O <hackerman.ant@gmail.com>
2023-09-28 13:25:11 +0100

    compareWith method added

--- phase_2/openWeatherAPI/weather.js
+++ phase_2/openWeatherAPI/weather.js
@@ -5,6 +5,7 @@ class Weather {
 
   async load(city) {
     this.data = await this.client.fetchWeatherData(city);
+    this.city = city;
   }
 
   getWeatherData() {
@@ -19,6 +20,18 @@ class Weather {
     console.log(`Weather: ${this.data.weather[0].description}`);
     console.log(`Temperature: ${this.data.main.temp}`);
     console.log(`Wind: ${this.data.wind.speed}`);
+  }
+
+  async compareWith(cityTwo) {
+    this.dataTwo = await this.client.fetchWeatherData(cityTwo);
+    this.cityTwo = cityTwo;
+    if (this.data.main.temp > this.dataTwo.main.temp) {
+      console.log(`${this.city} (${this.data.main.temp}) is warmer than ${cityTwo} (${this.dataTwo.main.temp})`);
+    } else if (this.data.main.temp < this.dataTwo.main.temp) {
+      console.log(`${this.cityTwo} (${this.dataTwo.main.temp}) is warmer than ${this.city} ((${this.data.main.temp}))`);
+    } else {
+      console.log(`${this.city} ((${this.data.main.temp})) is the same temperature as ${cityTwo} ((${this.dataTwo.main.temp}))`);
+    }
   }
 }
 
